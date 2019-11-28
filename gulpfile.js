f5cbfef8f2fd0d8eb8fc51cecdc8c0c9dae5fb26
DaniilFilippov <filippovdaniil14@gmail.com>
2019-11-28 23:22:26 +0300

    modified gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,29 @@
-'use strict';
+"use strict";
 
 var gulp = require("gulp");
-var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
-gulp.task('sass', function(){
-  gulp.src('source/sass/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('source/css/'));
-});
-gulp.task('watch', function(){
-  gulp.watch('source/sass/**/*.scss',['sass']);
-});
-gulp.task('default', ['sass','watch']);
-
-gulp.task("style", function() {
-  gulp.src("source/sass/style.scss")
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
     .pipe(plumber())
+    .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer({ overrideBrowserslist: ['last 2 versions']})
+      autoprefixer()
     ]))
+    .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("server", function () {
   server.init({
-    server: ".",
-    index: "source/index.html",
+    server: "source/",
     browser: 'chrome',
     notify: false,
     open: true,
@@ -39,6 +31,8 @@ gulp.task("serve", ["style"], function() {
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
+
+gulp.task("start", gulp.series("css", "server"));
